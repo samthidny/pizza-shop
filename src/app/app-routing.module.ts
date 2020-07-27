30f68990789b1e6d0e88378734a1ed2c65bb2c30
samthidny <andy.smith6@britishgas.co.uk>
2020-07-27 21:19:32 +0100

    initial bare bones app with routing and basic styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { DealsComponent } from './deals.component';
+import { MenuComponent } from './menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'deals', component: DealsComponent },
+  { path: 'menu', component: MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { DealsComponent } from './deals.component';
+import { MenuComponent } from './menu.component';
+import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DealsComponent,
+    MenuComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
